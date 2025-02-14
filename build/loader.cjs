b41e86f338f8b5700362a7d627ae1ce69b78445f
nick-one <nick-one@unknown.com>
2025-02-14 13:03:58 +0300

    Added loaders cjs/mjs

--- build/loader.cjs
+++ build/loader.cjs
@@ -1,2 +1,13 @@
-const yaml = require('yaml');
 const fs = require('node:fs');
+const yaml = require('yaml');
+
+require.extensions['.yml'] = function (module, filename) {
+    const file = fs.readFileSync(filename, 'utf8')
+
+    try {
+        module.exports = yaml.parse(file);
+    } catch (err) {
+        console.error(err.message);
+        throw err;
+    }
+}
--- build/loader.mjs
+++ build/loader.mjs
@@ -1,3 +1,27 @@
 import yaml from 'yaml';
 import path from 'node:path';
 import fs from 'node:fs/promises';
+
+const TARGET_EXT = '.yml';
+
+export async function load(url, context, nextLoad) {
+    const ext = path.extname(url);
+
+    if (ext !== TARGET_EXT) return nextLoad(url);
+
+    try {
+        const fileUrl = new URL(url).pathname;
+        const file = await fs.readFile(fileUrl, { encoding: 'utf-8' })
+
+        const parsedToJson = JSON.stringify(yaml.parse(file));
+
+        return {
+            format: 'json',
+            shortCircuit: true,
+            source: parsedToJson,
+        }
+    } catch (error) {
+        error.message = url + ': ' + error.message;
+        throw error;
+    }
+}
